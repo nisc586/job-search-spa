e7795ce189cf980e5b5f5be1ecae0b62d881eeb2
nisc586 <49026680+nisc586@users.noreply.github.com>
2024-11-03 23:41:55 +0100

    Save response content to file

--- fetch_test.js
+++ fetch_test.js
@@ -1,7 +1,23 @@
-fetch("https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/jobs", {
-    headers: {
-        "content-type": "application/json",
-        "X-API-Key": "jobboerse-jobsuche"}
-    })
-    .then((response) => console.log(response))
-    .catch((error) => console.error(error));
+import fs from "node:fs";
+
+let response;
+
+try {
+    response = await fetch("https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/jobs", {
+        headers: {
+            "content-type": "application/json",
+            "X-API-Key": "jobboerse-jobsuche"}
+        })
+} catch (e) {
+    console.error(e);
+}
+
+try {
+    if (response.ok) {
+        let content = await response.json()
+        fs.writeFileSync("jobsuche.json", JSON.stringify(content))
+    }
+} catch (e) {
+    console.error(e);
+}
+
